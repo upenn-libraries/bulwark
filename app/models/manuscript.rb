9744056dc7e0a958c028ed608f383c90f4392c2e
Katherine Lynch <katherly@upenn.edu>
2015-12-11 11:06:03 -0500

    PCDM behavior on Manuscripts

--- app/models/manuscript.rb
+++ app/models/manuscript.rb
@@ -1,4 +1,7 @@
 class Manuscript < ActiveFedora::Base
+
+  include Hydra::PCDM::ObjectBehavior
+
   property :title, predicate: ::RDF::Vocab::DC.title, multiple: false do |index|
     index.as :stored_searchable, :facetable
   end
