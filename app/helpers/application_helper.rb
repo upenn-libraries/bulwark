eeabe8ed49abe5df11d78145691fee053248cc4f
Katherine Lynch <katherly@upenn.edu>
2015-12-14 10:06:27 -0500

    Removed helper functions and references

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,3 @@
 module ApplicationHelper
 
-  def render_document_object_partials doc
-    fetch_files(doc['member_ids_ssim'])
-  end
-
-  def fetch_files(page_id)
-    page = ''
-    page_id.each do |p|
-      #page = Page.find(p)
-      #render_pages(page)
-    end
-  end
-
-  def render_pages(page)
-    puts page.text
-  end
-
 end
