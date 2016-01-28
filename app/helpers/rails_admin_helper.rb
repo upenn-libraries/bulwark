695e32ce3f549a373486ac3ceb41d804ffa0abd4
Katherine Lynch <katherly@upenn.edu>
2016-01-28 13:57:18 -0500

    Some CSS changes, light helper modification

--- app/helpers/rails_admin_helper.rb
+++ app/helpers/rails_admin_helper.rb
@@ -1,15 +1,15 @@
 module RailsAdminHelper
 
+  @filesystem_yml = "#{Rails.root}/config/filesystem.yml"
+  @@fs_config = YAML.load_file(File.expand_path(@filesystem_yml, __FILE__))
   def render_git_remote_options
-    header = content_tag("h2", "Git Actions")
     full_path = "/fs/pub/data/#{@object.directory}"
     page_content = content_tag("div", :class => "git-actions") do
-      concat(header)
       if Dir.exists?(full_path)
-        initialized_p = content_tag("p","Your git remote has been initialized at #{full_path}.  To begin using this remote, type the following into your terminal:")
-        initialized_pre = content_tag("pre", "git remote add origin /fs/pub/data/#{@object.directory}")
+        initialized_p = content_tag("p","Your git remote has been initialized at #{full_path}.  To begin using this remote, run the following commands from the terminal:")
+        initialized_pre = content_tag("pre", "git annex init\ngit remote add fs ~/#{@object.directory}")
         push_p = content_tag("p","To push to this remote, run the following command from the terminal:")
-        push_pre = content_tag("pre","git push origin master")
+        push_pre = content_tag("pre","git push fs master")
         concat(initialized_p)
         concat(initialized_pre)
         concat(push_p)
