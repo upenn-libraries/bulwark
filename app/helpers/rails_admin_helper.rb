a8d170e645929cf2d3008e9936a81fa6be4af535
Katherine Lynch <katherly@upenn.edu>
2016-01-28 13:27:58 -0500

    Ugly PoC for rendering git options helper

---
+++ app/helpers/rails_admin_helper.rb
@@ -0,0 +1,24 @@
+module RailsAdminHelper
+
+  def render_git_remote_options
+    header = content_tag("h2", "Git Actions")
+    full_path = "/fs/pub/data/#{@object.directory}"
+    page_content = content_tag("div", :class => "git-actions") do
+      concat(header)
+      if Dir.exists?(full_path)
+        initialized_p = content_tag("p","Your git remote has been initialized at #{full_path}.  To begin using this remote, type the following into your terminal:")
+        initialized_pre = content_tag("pre", "git remote add origin /fs/pub/data/#{@object.directory}")
+        push_p = content_tag("p","To push to this remote, run the following command from the terminal:")
+        push_pre = content_tag("pre","git push origin master")
+        concat(initialized_p)
+        concat(initialized_pre)
+        concat(push_p)
+        concat(push_pre)
+      else
+        page_content = link_to("Create Remote","http://127.0.0.1:3000/repos/#{@object.id}")
+      end
+    end
+    return page_content
+  end
+
+end
