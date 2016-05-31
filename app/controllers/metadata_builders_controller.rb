6f5db01ef7eb37a7df4c61e18821506ac0e1be00
Katherine Lynch <katherly@upenn.edu>
2016-05-31 10:53:46 -0400

    Updating multiple objects with nested attributes

--- app/controllers/metadata_builders_controller.rb
+++ app/controllers/metadata_builders_controller.rb
@@ -2,8 +2,6 @@ class MetadataBuildersController < ApplicationController
 
   before_action :_set_metadata_builder, only: [:show, :edit, :update, :ingest, :set_source, :set_preserve, :clear_files]
 
-  before_save :set_metadata_source_params
-
   def show
   end
 
@@ -17,9 +15,7 @@ class MetadataBuildersController < ApplicationController
 
   def update
     if @metadata_builder.update(metadata_builder_params)
-      @metadata_builder.metadata_source.each do |source|
-        # set params?
-      end
+      _update_metadata_sources if params[:metadata_builder][:metadata_source_attributes].present?
       redirect_to "#{root_url}admin_repo/repo/#{@metadata_builder.repo.id}/preview_xml", :flash => { :success => "Metadata mappings successfully updated.  See XML preview below."}
     else
       redirect_to "#{root_url}admin_repo/repo/#{@metadata_builder.repo.id}/preview_xml", :flash => { :error => @error_message }
@@ -54,7 +50,16 @@ class MetadataBuildersController < ApplicationController
 
   def metadata_builder_params
     params.require(:metadata_builder).permit(:parent_repo,
-      :metadata_source_attributes => [:view_type, :num_objects, :x_start, :y_start, :x_stop, :y_stop, :original_mappings, :root_element, :parent_element, :user_defined_mappings, :children => []])
+      :metadata_source_attributes => [:id, :view_type, :num_objects, :x_start, :y_start, :x_stop, :y_stop, :original_mappings, :root_element, :parent_element, :user_defined_mappings, :children => []])
+  end
+
+  def _update_metadata_sources
+    params[:metadata_builder][:metadata_source_attributes].each do |a|
+      hash_params_strings = Hash[*Hash[*a.flatten(1)].values.flatten(0)]
+      hash_params = Hash[hash_params_strings.map{|k,v| [k.to_sym, v]}]
+      metadata_source = MetadataSource.find(hash_params[:id])
+      metadata_source.update(hash_params)
+    end
   end
 
 end
