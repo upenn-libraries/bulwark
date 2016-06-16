58c60b3b910344e5af505ae60d7c1035837909ce
Katherine Lynch <katherly@upenn.edu>
2016-06-16 09:49:31 -0400

    Evened spacing in JS file

--- app/assets/javascripts/rails_admin/custom/hide_reveal.js
+++ app/assets/javascripts/rails_admin/custom/hide_reveal.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
   });
 
 	$(".xml-preview-form").submit(function(){
-      $(".wait").removeClass("hide");
-      $(".xml-preview").addClass("hide");
+    $(".wait").removeClass("hide");
+    $(".xml-preview").addClass("hide");
 	});
 
 	$(".ingest-select-form").submit(function(){
-      $(".wait").removeClass("hide");
-      $(".ingest-dashboard").addClass("hide");
+    $(".wait").removeClass("hide");
+    $(".ingest-dashboard").addClass("hide");
 	});
 
 });
