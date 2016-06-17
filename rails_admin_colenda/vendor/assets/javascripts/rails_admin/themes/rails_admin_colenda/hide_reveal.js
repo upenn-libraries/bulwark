e814b64e1e6d3bb631baad95f0a2d24964787ba4
Katherine Lynch <katherly@upenn.edu>
2016-06-17 09:13:24 -0400

    Fixed jQuery bug for click event using delegation

--- rails_admin_colenda/vendor/assets/javascripts/rails_admin/themes/rails_admin_colenda/hide_reveal.js
+++ rails_admin_colenda/vendor/assets/javascripts/rails_admin/themes/rails_admin_colenda/hide_reveal.js
@@ -6,12 +6,12 @@ $(document).ready(function(){
     $(".ingest-dashboard").removeClass("hide");
   });
 
-	$("#xml_preview_submit").click(function(){
+	$("#xml_preview_submit").on("click", function(){
     $(".wait").removeClass("hide");
     $(".xml-preview").addClass("hide");
 	});
 
-	$("#ingest_select_submit").click(function(){
+	$("#ingest_select_submit").on("click", function(){
     $(".wait").removeClass("hide");
     $(".ingest-dashboard").addClass("hide");
 	});
