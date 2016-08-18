1b272722a69adc284ff0969bf3365b41c0c3ab4d
Katherine Lynch <katherly@upenn.edu>
2016-08-18 14:41:28 -0400

    Adding configs

--- config/initializers/bunny.rb
+++ config/initializers/bunny.rb
@@ -1,16 +1,15 @@
-Bunny.configure do |config|
-  fs_env_file = Rails.root.join("config", 'bunny.yml')
-
-  fail "Missing configuration file at: #{fs_env_file}." unless File.exist?(fs_env_file)
-
-  begin
-    fs_yml = YAML.load_file(fs_env_file)
-  rescue StandardError
-    raise("#{fs_env_file} was found, but could not be parsed.\n")
+module Bunny
+  def config
+    @config ||= config_yaml.with_indifferent_access
   end
 
-  if File.exists?(fs_env_file)
-    options = fs_yml.fetch(Rails.env).with_indifferent_access
-  end
+  private
 
+    def config_yaml
+      config_file = Rails.root.join("config", 'bunny.yml')
+      fail "Missing configuration file at: #{config_file}." unless File.exist?(config_file)
+      YAML.load(ERB.new(File.read(config_file)).result)[Rails.env]
+    end
+
+    module_function :config, :config_yaml
 end
