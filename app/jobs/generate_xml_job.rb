799597ec150c8c07a1e8ea59c07b0e6300e527fc
Katherine Lynch <katherly@upenn.edu>
2016-08-26 10:29:09 -0400

    Support user email addresses

--- app/jobs/generate_xml_job.rb
+++ app/jobs/generate_xml_job.rb
@@ -4,9 +4,10 @@ class GenerateXmlJob < ActiveJob::Base
 
   after_perform :relay_message
 
-  def perform(metadata_builder, root_url)
-    @root_url = root_url
+  def perform(metadata_builder, root_url, user_email)
     @metadata_builder = metadata_builder
+    @root_url = root_url
+    @user_email = user_email
     metadata_builder.build_xml_files
   end
 
@@ -14,7 +15,7 @@ class GenerateXmlJob < ActiveJob::Base
 
   def relay_message
     MessengerClient.client.publish("XML generated")
-    NotificationMailer.process_completed_email("Preservation XML Generated", "katherly@upenn.edu", "Preservation-level XML generated for #{@metadata_builder.repo.unique_identifier} has been generated and is ready for review.\n\nReview at: #{@root_url}admin_repo/repo/#{@metadata_builder.repo.id}/preview_xml").deliver_now
+    NotificationMailer.process_completed_email("Preservation XML Generated", @user_email, "Preservation-level XML generated for #{@metadata_builder.repo.unique_identifier} has been generated and is ready for review.\n\nReview at: #{@root_url}admin_repo/repo/#{@metadata_builder.repo.id}/preview_xml").deliver_now
 
   end
 
--- app/jobs/ingest_job.rb
+++ app/jobs/ingest_job.rb
@@ -4,9 +4,10 @@ class IngestJob < ActiveJob::Base
 
   after_perform :relay_message
 
-  def perform(metadata_builder, ingest_params, root_url)
-    @root_url = root_url
+  def perform(metadata_builder, ingest_params, root_url, user_email)
     @metadata_builder = metadata_builder
+    @root_url = root_url
+    @user_email = user_email
     metadata_builder.transform_and_ingest(ingest_params)
   end
 
@@ -14,7 +15,7 @@ class IngestJob < ActiveJob::Base
 
   def relay_message
     MessengerClient.client.publish("Ingest complete")
-    NotificationMailer.process_completed_email("Ingestion and Derivative Generation", "katherly@upenn.edu", "#{@metadata_builder.repo.unique_identifier} has been ingested and its derivatives generated.  It is ready for review.\n\nReview at: #{@root_url}admin_repo/repo/#{@metadata_builder.repo.id}/ingest").deliver_now
+    NotificationMailer.process_completed_email("Ingestion and Derivative Generation", @user_email, "#{@metadata_builder.repo.unique_identifier} has been ingested and its derivatives generated.  It is ready for review.\n\nReview at: #{@root_url}admin_repo/repo/#{@metadata_builder.repo.id}/ingest").deliver_now
 
   end
 
