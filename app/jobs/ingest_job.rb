3a4cb849e06a5afe04e02fb6472fafabce1d6c4e
Katherine Lynch <katherly@upenn.edu>
2016-08-31 15:10:53 -0400

    Localized mailers, fixed bugs in jobs

--- app/jobs/ingest_job.rb
+++ app/jobs/ingest_job.rb
@@ -16,7 +16,6 @@ class IngestJob < ActiveJobStatus::TrackableJob
   def relay_message
     MessengerClient.client.publish(I18n.t('rabbitmq.publish.messages.ingest'))
     NotificationMailer.process_completed_email(I18n.t('colenda.mailers.notification.ingest.subject'), @user_email, I18n.t('colenda.mailers.notification.ingest.body', :uuid => @metadata_builder.repo.unique_identifier, :root_url => @root_url, :link_fragment => @metadata_builder.repo.id)).deliver_now
-
   end
 
 end
