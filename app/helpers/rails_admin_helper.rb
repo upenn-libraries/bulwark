bbba34d1a85b7e9ebde800dfc7a629ced983410c
Katherine Lynch <katherly@upenn.edu>
2016-09-08 09:43:56 -0400

    Sprucing up views utilizing images_to_render

--- app/helpers/rails_admin_helper.rb
+++ app/helpers/rails_admin_helper.rb
@@ -56,32 +56,20 @@ module RailsAdminHelper
     return best_guess
   end
 
-  def attributes_display(object)
-    atrributes_for_display = {}
-    object.attribute_names.sort.each do |a_name|
-      if object.try(a_name).present?
-        items_for_display = []
-        Array[object.try(a_name)].flatten(1).each do |value|
-          items_for_display << value
-        end
-        atrributes_for_display[a_name.capitalize.to_sym] = items_for_display
-      end
-    end
+  def render_display_attributes(view_type, attributes, image_key = "")
     attributes_display = ""
-    atrributes_for_display.each do |key, values|
-      items = wrap_values(values)
+    attributes_display << content_tag(:h3, "#{view_type.capitalize} preview for #{identifier_selection(attributes)}")
+    attributes.each do |key, value|
+      items = wrap_values(value)
       attributes_display << content_tag(:strong, key)
       attributes_display << content_tag(:ul, items)
     end
-    return attributes_display
+    attributes_display << content_tag(:div, thumbnail_preview(image_key), :class => "thumbnail") if image_key.present?
+    return content_tag(:div, attributes_display.html_safe, :class => "fragment").html_safe
   end
 
-  def wrap_values(values)
-    formatted = ""
-    values.each do |value|
-      formatted << content_tag(:li, value.blank? ? "N/A" : value)
-    end
-    return formatted.html_safe
+  def wrap_values(value)
+    return content_tag(:li, value.blank? ? "N/A" : Array(value).join(", ") ).html_safe
   end
 
   def form_label(form_type, repo_steps)
@@ -117,6 +105,14 @@ module RailsAdminHelper
       ready_partial = "shared/generic_error"
     end
     (get_job_status(job_id) == :queued) || (get_job_status(job_id) == :working) ? "shared/waiting" : ready_partial
+  end
+
+  def identifier_selection(attributes)
+    return attributes["id"].present? ? attributes["id"] : "Object"
+  end
+
+  def thumbnail_preview(thumbnail_link)
+    image_tag(thumbnail_link, :width => 100, :height => 150)
   end
 
 end
