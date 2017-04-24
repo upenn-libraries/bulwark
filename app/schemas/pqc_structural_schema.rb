2aa619da8fdcc7c725bad03b20bc00ac90ded6dc
Katherine Lynch <katherly@upenn.edu>
2017-04-24 11:31:50 -0400

    Adding structural metadata from catalog

--- app/schemas/pqc_structural_schema.rb
+++ app/schemas/pqc_structural_schema.rb
@@ -4,19 +4,27 @@ class PqcStructuralSchema < ActiveTriples::Schema
     index.as :stored_searchable, :facetable
   end
 
-  property :parent_manuscript, predicate: ::RDF::URI.new('http://library.upenn.edu/pqc/ns/parentManuscript'), multiple: true do |index|
-    index.as :stored_searchable
+  property :sequence, predicate: ::RDF::URI.new('http://library.upenn.edu/pqc/ns/sequence'), multiple: true do |index|
+    index.as :displayable
   end
 
   property :page_number, predicate: ::RDF::URI.new('http://library.upenn.edu/pqc/ns/pageNumber'), multiple: true do |index|
+    index.as :displayable
+  end
+
+  property :hinge, predicate: ::RDF::URI.new('http://library.upenn.edu/pqc/ns/hinge'), multiple: true do |index|
+    index.as :displayable
+  end
+
+  property :side, predicate: ::RDF::URI.new('http://library.upenn.edu/pqc/ns/side'), multiple: true do |index|
+    index.as :displayable
+  end
+
+  property :toc_entry, predicate: ::RDF::URI.new('http://library.upenn.edu/pqc/ns/tocEntry'), multiple: false do |index|
     index.as :stored_searchable
   end
 
   property :file_name, predicate: ::RDF::URI.new('http://library.upenn.edu/pqc/ns/fileName'), multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :page_id, predicate: ::RDF::Vocab::DC.identifier, multiple: true do |index|
     index.as :stored_searchable
   end
 
