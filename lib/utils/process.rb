245e91a5fb88699b9566beb554142143a757b1bc
Katherine Lynch <katherly@upenn.edu>
2017-12-07 15:44:10 -0500

    Updated payload save for phalt

--- lib/utils/process.rb
+++ lib/utils/process.rb
@@ -129,7 +129,7 @@ module Utils
       end
 
       repo.images_to_render['iiif'] = { 'reading_direction' => repo.metadata_builder.determine_reading_direction,
-                                        'images' => display_array.map{ |s| "#{Display.config['iiif']['image_server']}/#{repo.names.bucket}%2F#{s}/info.json" }
+                                        'images' => display_array.map{ |s| "#{Display.config['iiif']['image_server']}#{repo.names.bucket}%2F#{s}/info.json" }
       }
       repo.save!
     end
