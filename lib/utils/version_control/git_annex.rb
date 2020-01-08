e7b3bb04b017803e22668e72294edfabb9ed5bab
Kate Lynch <katherly@upenn.edu>
2020-01-08 14:17:04 -0500

    Modify user permissions for git-annex

--- lib/utils/version_control/git_annex.rb
+++ lib/utils/version_control/git_annex.rb
@@ -121,6 +121,7 @@ module Utils
         Dir.chdir(Rails.root.to_s)
         parent_dir = dir.gsub(repo.names.git,"")
         FileUtils.rm_rf(parent_dir, :secure => true) if File.directory?(parent_dir)
+        change_bare_repo_permissions if Rails.env.production?
       end
 
       def get(options, dir)
@@ -243,6 +244,9 @@ module Utils
         "#{Utils.config[:workspace]}/#{path_seed}"
       end
 
+      def change_bare_repo_permissions
+        `chown -R #{ENV['IMAGING_USER']}:#{ENV['IMAGING_USER']} #{Utils.config["assets_path"]}/#{repo.names.git}` if ENV['IMAGING_USER']
+      end
 
     end
   end
