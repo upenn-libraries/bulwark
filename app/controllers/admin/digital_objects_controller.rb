fc9c381a242aeaf038b5effce37cfe426a8bbb54
Carla Galarza <cgalarza@upenn.edu>
2021-02-08 13:11:44 -0500

    Order digital objects so that the most recently created digital objects are at the top of the page

--- app/controllers/admin/digital_objects_controller.rb
+++ app/controllers/admin/digital_objects_controller.rb
@@ -2,7 +2,7 @@ module Admin
   class DigitalObjectsController < AdminController
     def index
       # Querying for repos created in the new format.
-      @digital_objects = Repo.new_format
+      @digital_objects = Repo.new_format.order(created_at: :desc)
       @digital_objects = @digital_objects.id_search(params[:id_search]) if params[:id_search].present?
       @digital_objects = @digital_objects.name_search(params[:name_search]) if params[:name_search].present?
       @digital_objects = @digital_objects.where(owner: params[:owner_search]) if params[:owner_search].present?
