0fb28b295ccd04f516baab153f3c2d9f4a729652
Carla Galarza <cgalarza@upenn.edu>
2021-03-19 12:07:11 -0400

    Adding ability to filter by published

--- app/controllers/admin/digital_objects_controller.rb
+++ app/controllers/admin/digital_objects_controller.rb
@@ -4,10 +4,11 @@ module Admin
   class DigitalObjectsController < AdminController
     def index
       # Querying for repos created in the new format.
-      @digital_objects = Repo.new_format.order(created_at: :desc).page(params[:page]).per(params[:per_page])
+      @digital_objects = Repo.new_format.includes(:created_by).order(created_at: :desc).page(params[:page]).per(params[:per_page])
       @digital_objects = @digital_objects.id_search(params[:id_search]) if params[:id_search].present?
       @digital_objects = @digital_objects.name_search(params[:name_search]) if params[:name_search].present?
       @digital_objects = @digital_objects.where(created_by_id: params[:created_by_search]) if params[:created_by_search].present?
+      @digital_objects = @digital_objects.where(published: params[:published_filter]) if params[:published_filter].present?
     end
 
     def show
