99bf526c09d348e20a772c1370351fe711f6aa87
Carla Galarza <cgalarza@upenn.edu>
2021-07-01 16:16:55 -0400

    Converting EZID password to docker secret

--- config/initializers/ezid.rb
+++ config/initializers/ezid.rb
@@ -1,5 +1,5 @@
 Ezid::Client.configure do |conf|
   conf.default_shoulder = 'ark:/99999/fk4' unless ENV['EZID_DEFAULT_SHOULDER']
   conf.user = 'apitest' unless ENV['EZID_USER']
-  conf.password = 'apitest' unless ENV['EZID_PASSWORD']
+  conf.password = File.exist?('/run/secrets/ezid_password') ? File.read('/run/secrets/ezid_password').strip : 'apitest'
 end
