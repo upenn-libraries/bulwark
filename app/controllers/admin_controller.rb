a61ea0e24e74f5e960a13bbbd3924b0a9c898eb2
Carla Galarza <cgalarza@upenn.edu>
2021-07-20 11:20:09 -0400

    Adding alert header to administrative pages; Moving alert header to very top of the page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,10 @@
 class AdminController < ActionController::Base
   before_action :authenticate_user!
+  before_action :header_alert
 
   layout 'admin'
+
+  def header_alert
+    @header_alert = AlertMessage.header
+  end
 end
