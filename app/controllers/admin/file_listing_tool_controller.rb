7d8a0e97b5149cb64d544f87ed19772495005f2f
Carla Galarza <cgalarza@upenn.edu>
2021-11-10 15:44:47 -0500

    Cleaning up code and adding the ability to download csv containing filenames

--- app/controllers/admin/file_listing_tool_controller.rb
+++ app/controllers/admin/file_listing_tool_controller.rb
@@ -7,25 +7,27 @@ module Admin
     def file_list
       respond_to do |format|
         if valid_path?
-          format.json { render json: { filenames: filenames(absolute_path), drive: params[:drive], path: params[:path] }, status: :ok }
+          format.csv { send_data csv, type: 'text/csv', filename: 'structural_metadata.csv', disposition: :download }
+          format.json { render json: { filenames: filenames.join('; '), drive: params[:drive], path: params[:path] }, status: :ok }
         else
-          format.json { render json: { error: 'Error with path' }, status: :unprocessable_entity }
+          format.csv { }
+          format.json { render json: { error: 'Path invalid!' }, status: :unprocessable_entity }
         end
       end
     end
 
-    def csv
-      return unless valid_path?
-
-      data = filenames.map { |f| { filename: f } }
-      csv = Bulwark::StructuredCSV.generate(data)
-      send_data csv, type: 'text/csv', filename: 'structural_metadata.csv', disposition: :download
-    end
-
     private
 
+      def csv
+        data = filenames.map { |f| { filename: f } }
+        Bulwark::StructuredCSV.generate(data)
+      end
+
       def valid_path?
-        params[:path].present? && absolute_path.starts_with?(Bulwark::Import::MountedDrives.path_to(params[:drive]))
+        params[:path].present? &&
+          Bulwark::Import::MountedDrives.valid?(params[:drive]) &&
+          absolute_path.starts_with?(Bulwark::Import::MountedDrives.path_to(params[:drive])) &&
+          File.exist?(absolute_path)
       end
 
       def absolute_path
@@ -33,8 +35,8 @@ module Admin
         File.expand_path(File.join(path_to_drive, params[:path]))
       end
 
-      def filenames(path)
-        Dir.entries(path).select { |f| !f.start_with?('.') }.sort.join('; ')
+      def filenames
+        Dir.entries(absolute_path).select { |f| !f.start_with?('.') }.sort
       end
   end
 end
