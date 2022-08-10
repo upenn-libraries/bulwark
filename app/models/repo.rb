21e94edfb76ceaf5962e6e690e7cabf149f9377c
Carla Galarza <cgalarza@upenn.edu>
2022-08-10 11:37:58 -0400

    Adding has_image field to Solr document for future use

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -288,7 +288,8 @@ class Repo < ActiveRecord::Base
       'system_create_dtsi' => first_published_at.utc.iso8601,
       'system_modified_dtsi' => last_published_at.utc.iso8601,
       'thumbnail_location_ssi' => thumbnail_location,
-      'bibnumber_ssi' => bibid
+      'bibnumber_ssi' => bibid,
+      'has_images_bsi' => has_images? ? 'T' : nil,
     }
 
     MetadataSource::VALID_DESCRIPTIVE_METADATA_FIELDS.each do |field|
