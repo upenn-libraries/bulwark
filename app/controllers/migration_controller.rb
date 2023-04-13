a5d05cf4e54f4048d4f1a4837da5bbec991bb089
Mike Kanning <mkanning@upenn.edu>
2023-04-13 15:19:55 -0400

    add controller error handling and corresponding request specs. update rubocop "todo" ¯\_(ツ)_/¯

--- app/controllers/migration_controller.rb
+++ app/controllers/migration_controller.rb
@@ -3,12 +3,19 @@
 class MigrationController < ActionController::Base
   # before_action :authenticate_token! # TODO: ???
 
-  # rescue_from 'ActiveRecord::RecordNotFound' # TODO: handle
-  # rescue_from 'MigrationObjectBuilder::Error' # TODO: handle
+  rescue_from 'ActiveRecord::RecordNotFound' do
+    render json: { message: 'Record cannot be found' }, status: 404
+  end
+
+  rescue_from 'MigrationObjectBuilder::Error' do |exception|
+    render json: { message: 'Problem generating serialized representation of repo',
+                   exception: exception.message },
+           status: 500
+  end
 
   # TODO: check if already migrated?
   def serialize
-    repo = Repo.find_by unique_identifier: CGI.unescape(params[:id])
+    repo = Repo.find_by! unique_identifier: CGI.unescape(params[:id])
     migration_hash = MigrationObjectBuilder.new(repo).build
     render json: migration_hash.to_json # TODO: use Oj?
   end
--- spec/requests/migration_spec.rb
+++ spec/requests/migration_spec.rb
@@ -3,10 +3,24 @@
 RSpec.describe 'Migration Endpoints', type: :request do
   let(:repo) { FactoryBot.create :repo, :with_unique_identifier }
 
-  describe "GET migration#serialize" do
+  describe 'GET migration#serialize' do
     it 'returns OK' do
       get serialize_path(CGI.escape(repo.unique_identifier)), format: :json
       expect(response).to have_http_status(200)
+    end
+
+    it 'returns 404 if repo not found' do
+      get serialize_path('some-bogus-id'), format: :json
+      expect(response).to have_http_status(404)
+    end
+
+    it 'returns 500 if error during object build' do
+      allow_any_instance_of(MigrationObjectBuilder).to(
+        receive(:build).and_raise(MigrationObjectBuilder::Error.new('Kaboom'))
+      )
+      get serialize_path(CGI.escape(repo.unique_identifier)), format: :json
+      expect(response).to have_http_status(500)
+      expect(JSON.parse(response.body)['exception']).to eq 'Kaboom'
     end
   end
 end
