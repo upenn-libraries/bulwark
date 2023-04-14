81a3c1faf071b7f89b5ec1c6ac275ba9458690a7
Mike Kanning <mkanning@upenn.edu>
2023-04-14 16:27:02 -0400

    fix double-nested route definition. set checksum value. clean up.

--- app/models/migration_object_builder.rb
+++ app/models/migration_object_builder.rb
@@ -4,6 +4,8 @@
 class MigrationObjectBuilder
   class Error < StandardError; end
 
+  EXPECTED_SHA_LENGTH = 64
+
   # @param [Repo] repo
   def initialize(repo)
     @repo = repo
@@ -50,34 +52,39 @@ class MigrationObjectBuilder
 
   # @return [Hash]
   def assets
-    {
-      bucket: @repo.names.bucket,
+    { bucket: @repo.names.bucket,
       arranged: arranged_assets,
-      unarranged: unarranged_assets
-    }
+      unarranged: unarranged_assets }
   end
 
   def arranged_assets
     source = @structural_metadata.original_mappings['sequence']
     source.map do |seq|
       asset = @assets.find { |a| a.filename == seq['filename'] }
-      {
-        filename: seq['filename'], # original filename - or is this a value from the asset?
+      { filename: seq['filename'],
         label: seq['label'],
         annotations: seq['table_of_contents'],
-        transcription: seq['fulltext'], # not set by test factory...
-        checksum: '', # TODO: pull jhove output file from git repo? aughhhh
-        path: "#{@repo.names.bucket}%2F#{asset.access_file_location}" # "path to file in the bucket, won't actually be a path just a filename"
-      }
+        transcription: seq['fulltext'],
+        checksum: asset_checksum(asset),
+        path: "#{@repo.names.bucket}%2F#{asset.access_file_location}" }
     end
   end
 
+  # assuming @asset entries not in sequence are un-arranged
   # @return [Array]
   def unarranged_assets
-    # assuming @asset entries not in sequence
     source = Array.wrap(@structural_metadata.original_mappings['sequence'])
     asset_filenames = Array.wrap(@assets.map(&:filename))
     seq_filenames = source.map { |s| s['filename'] }
     asset_filenames - seq_filenames
+  end
+
+  # extract sha256 value from original_file_location
+  # @param [Asset] asset
+  # @return [String,nil]
+  def asset_checksum(asset)
+    filename = asset.original_file_location
+    sha = Array.wrap(filename.split('-')).last.split('.')&.first
+    sha&.length == EXPECTED_SHA_LENGTH ? sha : nil # TODO: raise if no sha available?
   end
 end
