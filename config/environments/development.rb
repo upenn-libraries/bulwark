2f7f408d839bf0f5042986aa77a287230a48aae2
Carla Galarza <cgalarza@upenn.edu>
2024-02-08 23:45:14 -0500

    Configuring default host outside of config block

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,9 +39,9 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  # Default host to use for generating links outside of views
-  routes.default_url_options[:host] = 'localhost:3000'
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
+# Default host to use for generating links outside of views
+Rails.application.routes.default_url_options[:host] = 'localhost:3000'
