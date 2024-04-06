08ddd25e9abdd2bf619aac19c2d70f8b3244768f
Carla Galarza <cgalarza@upenn.edu>
2024-04-05 20:07:49 -0400

    Displaying access links for non-iiif files. Adding size to all links. Customizing access file download name.

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -8,6 +8,7 @@ class AssetsController < ActionController::Base
   class KeyNotFound < StandardError; end
 
   before_action :fetch_item
+  before_action :fetch_asset, except: [:thumbnail]
 
   rescue_from 'AssetsController::ItemNotFound', 'AssetsController::KeyNotFound' do |_e|
     head :not_found
@@ -19,13 +20,11 @@ class AssetsController < ActionController::Base
     client = client(config.to_h.except(:bucket))
 
     # Fetching file location from published json.
-    assets_json = @item.published_json['assets']
-    asset = assets_json&.find { |a| a['id'] == params['id'] }
-    key = asset&.dig('original_file', 'path')
+    key = @asset&.dig('original_file', 'path')
 
     raise KeyNotFound unless key
 
-    redirect_to presigned_url(client, config[:bucket], key, :attachment, asset['filename']),
+    redirect_to presigned_url(client, config[:bucket], key, :attachment, @asset['filename']),
                 status: :temporary_redirect
   end
 
@@ -40,10 +39,27 @@ class AssetsController < ActionController::Base
   def access
     config = Settings.derivative_storage # For now only displaying non-iiif access copies
     client = client(config.to_h.except(:bucket))
-    redirect_to presigned_url(client, config[:bucket], "#{params[:id]}/access"), status: :temporary_redirect
+
+    # Fetching file location from published json.
+    key = @asset&.dig('access_file', 'path')
+
+    raise KeyNotFound unless key
+
+    access_ext = Mime::Type.lookup(@asset&.dig('access_file', 'mime_type')).symbol
+    access_filename = "#{File.basename(@asset['filename'], '.*')}.#{access_ext}"
+
+    redirect_to presigned_url(client, config[:bucket], key, :attachment, access_filename),
+                status: :temporary_redirect
   end
 
   private
+
+    # Fetch published json for asset.
+    # @return [Hash]
+    def fetch_asset
+      assets_json = @item.published_json['assets']
+      @asset = assets_json&.find { |a| a['id'] == params['id'] }
+    end
 
     def fetch_item
       @item = Item.find_by(unique_identifier: params[:item_id])
